bde504d332b525723b098c1613ede872c6757c25
GEd520 <3330253407@qq.com>
2025-04-02 03:51:27 +0800

    Update qiandao.py

--- .github/scripts/qiandao.py
+++ .github/scripts/qiandao.py
@@ -1,89 +1,89 @@
-#-*-编码：utf-8-*-
-进口操作系统
-进口再
-进口sys
-进口请求
-从……起BS4进口Beautiful汤
-从……起urllib.parse进口urlparse
-进口xml.etree.ElementTree作为和
-进口采运作业
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import xml.etree.ElementTree as ET
+import logging
 
-记录器=记录。getLogger(__name__)
-记录器。SetLevel(采运作业。调试)
-格式化程序=记录。格式化程序("%(actime)s-%(name)s-%(levelName)s-%(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-Ch=记录。StreamHandler(流=sys。stdout)
-Ch.SetLevel(采运作业。信息)
-Ch.setFormatter(格式化程序)
-记录器。addHandler(Ch)
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
-定义 getXmlCdata(身体):
-    返回和。fromString(str(身体)).文本
+def getXmlCdata(body):
+    return ET.fromstring(str(body)).text
 
-班级BBSClient：
-    定义 __init__(self，主机名：str，用户名：str，密码：str，问题ID:str='0'，答案：str=没有一个，代理：dict|没有一个=没有一个):
-自己。会议=请求。会议()
-自己。主机名=主机名
-自己。用户名=用户名
-自己。密码=密码
-自己。questionid=questionid
-自己。回答=答案
-自己。_common_headers={
-            "主机":f"{主机名}",
-            "连接":"保持活动状态",
-            "接受":"text/html，application/xhtml+xml，application/xml；q=0.9，image/Avif，image/webp，image/APNG，*/*；q=0。8，应用程序/签名交换；v=b3；q=0.7",
-            "用户代理":"Mozilla/5.0(X11；Linux x86_64)AppleWebKit/537.36(KHTML，如Gecko)Chrome/126.0.0.0 Safari/537.36",
-            "接受语言":"zh-CN，cn；q=0.9",
-            "内容类型":"应用程序/x-www-form-urlencoded",
+class BBSClient:
+    def __init__(self, hostname: str, username: str, password: str, questionid: str = '0', answer: str = None, proxies: dict | None = None):
+        self.session = requests.Session()
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.questionid = questionid
+        self.answer = answer
+        self._common_headers = {
+            "Host": f"{hostname}",
+            "Connection": "keep-alive",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "Accept-Language": "zh-CN,cn;q=0.9",
+            "Content-Type": "application/x-www-form-urlencoded",
         }
-自己。代理=代理
-自己。formhash=''
+        self.proxies = proxies
+        self.formhash = ''
 
-    定义 login_form_hash(自己):
-响应=自我。会议.得到(F'https://{自己。主机名}/member.php？mod=记录&action=登录').文本
-loginhash=re。搜索(R'<div id="main_messaqge_(.+？)">'，响应).组(1)
-formhash=re.搜索(R'<输入类型="hidden"name="formhash"value="(.+？)"/>'，响应).组(1)
-        返回loginhash，formhash
+    def login_form_hash(self):
+        response = self.session.get(f'https://{self.hostname}/member.php?mod=logging&action=login').text
+        loginhash = re.search(r'<div id="main_messaqge_(.+?)">', response).group(1)
+        formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', response).group(1)
+        return loginhash, formhash
 
-    定义 登录(自己):
-        """使用用户名和密码登录"""
-loginhash，formhash=self。login_form_hash()
-login_url=F'https://{自己。主机名}/member.php？mod=logging&action=login&loginsubmit=yes&loginhash={loginhash}inajax=1(&I)‘
-标题={**自己。_common_headers,"来源":F'https://{自己。主机名}',"referer":F'https://{自己。主机名}/'}
-有效负载={
-            'formhash'：formhash，
-            'referer':F'https://{自己。主机名}/',
-            '用户名'：self。用户名,
-            '密码'：self。密码,
-            'questionid'：self。questionid,
-            '答案'：self。回答,
+    def login(self):
+        """Login with username and password"""
+        loginhash, formhash = self.login_form_hash()
+        login_url = f'https://{self.hostname}/member.php?mod=logging&action=login&loginsubmit=yes&loginhash={loginhash}&inajax=1'
+        headers = {**self._common_headers, "origin": f'https://{self.hostname}', "referer": f'https://{self.hostname}/'}
+        payload = {
+            'formhash': formhash,
+            'referer': f'https://{self.hostname}/',
+            'username': self.username,
+            'password': self.password,
+            'questionid': self.questionid,
+            'answer': self.answer,
         }
 
-响应=自我。会议.邮件(login_url，proxies=self。代理，data=有效负载，header=header)
-        如果响应。status_code==200:
-记录器。信息(F'Welcome{自己。用户名}!')
-自己。get_form_hash()
-        其他:
-            提高 ValueError('验证失败！检查您的用户名和密码！')
+        response = self.session.post(login_url, proxies=self.proxies, data=payload, headers=headers)
+        if response.status_code == 200:
+            logger.info(f'Welcome {self.username}!')
+            self.get_form_hash()
+        else:
+            raise ValueError('Verify Failed! Check your username and password!')
 
-    定义 get_form_hash(自己):
-响应=自我。会议.得到(F'https://{自己。主机名}/home.php').文本
-自己。formhash=re.搜索(R'formhash=([\da-f]+)'，响应).组(1)
+    def get_form_hash(self):
+        response = self.session.get(f'https://{self.hostname}/home.php').text
+        self.formhash = re.search(r'formhash=([\da-f]+)', response).group(1)
 
-    定义 签入(自己):
-checkin_url=F'https://{自己。主机名}/plugin.php？id=k_MiSign:sign&operation=qiandao&formhash={自己。formhash}inajax=1(&I)‘
-        返回自己。会议.得到(checkin_url).文本
+    def checkin(self):
+        checkin_url = f'https://{self.hostname}/plugin.php?id=k_misign:sign&operation=qiandao&formhash={self.formhash}&inajax=1'
+        return self.session.get(checkin_url).text
 
-    定义 信用(自己):
-credit_url=F"https://{自己。主机名}/home.PHP？mod=spaccp&ac=credit&showcredit=1&inajax=1&ajaxarget=excredit菜单"
-credit_response=self。会议.得到(credit_url).文本
+    def credit(self):
+        credit_url = f"https://{self.hostname}/home.php?mod=spacecp&ac=credit&showcredit=1&inajax=1&ajaxtarget=extcreditmenu_menu"
+        credit_response = self.session.get(credit_url).text
 
-        #解析XML，提取CDATA
-CDATA内容=getXmlCdata(credit_response)
+        # 解析 XML，提取 CDATA
+        cdata_content = getXmlCdata(credit_response)
 
-        #使用Beautiful Soup解析CDATA内容
-CDATA_soup=Beautiful汤(CDATA内容，功能="lxml")
-credits=cdata_soup。选择('ul li')
+        # 使用 BeautifulSoup 解析 CDATA 内容
+        cdata_soup = BeautifulSoup(cdata_content, features="lxml")
+        credits = cdata_soup.select('ul li')
         hcredits = '\n'.join(hcredit.text for hcredit in credits)
         return hcredits
 
@@ -109,4 +109,4 @@ if __name__ == '__main__':
         logger.info(client.logout())
     except Exception as e:
         logger.error(e)
-sys.exit(1)
+        sys.exit(1)
